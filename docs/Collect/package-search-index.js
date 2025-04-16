1db0d4ffeef3d90ab3aff3b2f8fd142660c8ac50
John-Ryan Dunn <socraticphoenix@gmail.com>
2025-04-16 10:45:44 -0400

    Update javadocs, gradle, and CI

---
+++ docs/Collect/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"honeyroasted.collect.copy"},{"l":"honeyroasted.collect.equivalence"},{"l":"honeyroasted.collect.modify"},{"l":"honeyroasted.collect.multi"},{"l":"honeyroasted.collect.property"}];updateSearchResults();
